f9cf990a21bbdc3da8d38313efabacac42bb026e
Kenneth <quibelkenneth@gmail.com>
2024-05-01 09:45:06 +0800

    adjust the size of techstack and techskills for mobile

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -34,51 +34,51 @@ function Techstack() {
     <>
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <b className="purple">Web Development</b>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <DiJavascript1 />
         <p className="tech-names">Javascript</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <DiNodejs />
         <p className="tech-names">Node.js</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <DiReact />
         <p className="tech-names">React</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiNextdotjs />
         <p className="tech-names">Next.js</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiTailwindcss />
         <p className="tech-names">Tailwind Css</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiHtml5 />
         <p className="tech-names">HTML</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiCss3 />
         <p className="tech-names">CSS</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <DiPhp />
         <p className="tech-names">PHP</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <DiMongodb />
         <p className="tech-names">MongoDB</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiFirebase />
         <p className="tech-names">Firebase</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiMysql />
         <p className="tech-names">MySQL</p>
       </Col>
-      <Col xs={2} md={1} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <SiPrisma />
         <p className="tech-names">Prisma</p>
       </Col>
